8fd7a2d98f3889920192e413acad240fb41d2b76
vermillion <wolfenstein_88@mail.ru>
2018-05-27 21:50:06 +0300

    init project

---
+++ src/test/java/ru/max/tabs/TabsApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.max.tabs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TabsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
